4a55fcaa386a9acef1b2c84c11f89e216a8cc264
Vishal Gaikwad <154438441+gaikwad8vishal@users.noreply.github.com>
2025-03-08 01:42:14 +0530

    Delete brainly-fe2 directory

--- brainly-fe2/src/Errorr/Errorr.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export function Errorr(){
-    return <div className="flex items-center justify-center ">
-        <div className="text-4xl">
-            Not found
-        </div>
-    </div>
-}
